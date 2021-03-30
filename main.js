79aadfa92e8f09ac923856fa0cd2b334e5d975e0
Izurii <heitor_massarente@msn.com>
2021-03-30 14:18:48 -0300

    Initial communication between electron back-end and app

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-const { app, BrowserWindow } = require('electron')
+const { app, ipcMain, BrowserWindow } = require('electron');
 const path = require('path');
+const { setKeyboardOptions } = require('./driver/index');
+
+try {
+	require('electron-reloader')(module)
+} catch (_) {}
 
 let mainWindow;
 
 app.on('ready', () => {
 	mainWindow = new BrowserWindow({
 		webPreferences: {
-			preload: path.join(__dirname, 'preload.js')
+			nodeIntegration: true,
+			contextIsolation: false			
 		}
 	});
+	app.allowRendererProcessReuse = false;
 	mainWindow.loadFile(path.join(__dirname, 'index.html'));
-});
+}); 
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -22,4 +29,8 @@ app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow()
   }
+});
+
+ipcMain.on('setKB', (event, segmentOptions) => {
+	setKeyboardOptions(3, segmentOptions);
 });

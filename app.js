dbfac147a24c5f6e2c856bd1061d54259f8f0091
Izurii <heitor_massarente@msn.com>
2021-03-31 02:31:02 -0300

    Added keyboard made with html,css and selectable segments and colors changing

--- app.js
+++ app.js
@@ -72,10 +72,10 @@ const Colors = [
 
 var app = angular.module('app', ['ngRoute', 'ngAnimate', 'ngMaterial']);
 
-app.config(['$routeProvider', function ($routeProvider) {
-	
+app.filter('htmlTrusted', ['$sce', function($sce){
+	return function(text){ return $sce.trustAsHtml(text); };
 }])
-.controller('indexController', function ($scope) {
+.controller('indexController', function ($scope, $sce) {
 	
 	$scope.brightness = SegmentBrightness;
 	$scope.colors = Colors;
@@ -98,19 +98,44 @@ app.config(['$routeProvider', function ($routeProvider) {
 		ipcRenderer.send('setKB', $scope.segmentsOptions);
 	};
 
-	// 71 teclas iguais
-	// 16 teclas numpad
-	// 1 space
-	// 1 shifts
-	// 1 enter
-	// 1 backspapce
-	// 1 tab
-	// 1 caps lock
-	// 1 ctrl, alt gr, rec, ask and slash (right side) = 4
+	$scope.getKey = (keyWidth, keyRow, keyIndex, kOrn) => {
 
-	// -----
-	// 97 teclas
+		let colorOption = '#000000'
 
+		if(kOrn=='k') {
+			if(keyIndex < 4) keyRow = 0;
+			if(keyIndex >= 4 && keyIndex < 8) { if((keyRow!=5) || (keyRow==5&&keyIndex==4)) keyRow = 1; else keyRow=2; };
+			if(keyIndex >= 8 && keyIndex < 14) keyRow = 2;
+		} else if (kOrn=='n')
+			keyRow = 3;
 
+		if($scope.segmentsOptions[keyRow]!=undefined)
+			colorOption = Colors[$scope.segmentsOptions[keyRow].segmentColor].colorHex;
+		return $sce.trustAsHtml(`<span class="keys" style="width:${keyWidth} !important; border: 2px solid ${colorOption}"></span>`);
+	};
+
+	$scope.keyboard = [];
+	$scope.numpad = [];
+
+	let keys = [
+		['30px', '30px', '30px', '30px', '30px', '30px', '30px', '30px', '30px', '30px', '30px', '30px', '30px', '30px'],
+		['29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '40px'], // diff 0.7px => 30px => 29.3px
+		['40px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px', '29.27px'], // diff 0.7px => 30px => 29.3px
+		['45px', '29.69px', '29.69px', '29.69px', '29.69px', '29.69px', '29.69px', '29.69px', '29.69px', '29.69px', '29.69px', '29.69px', '55px'], // diff 3.3px => 30px => 29.43px
+		['60px', '30.3px', '30.3px', '30.3px', '30.3px', '30.3px', '30.3px', '30.3px', '30.3px', '30.3px', '30.3px', '70px'], // diff 0.15 => 30px => 29.85px
+		['40px', '30px', '30px', '30px', '161px', '40px', '40px', '40px', '40px']
+	];
+
+	let keys_numpad = [
+		['25px', '25px', '25px', '25px'],
+		['25px', '25px', '25px', '25px'],
+		['25px', '25px', '25px', '25px'],
+		['25px', '25px', '25px', '25px'],
+		['35px'],
+		['35px', '35px', '35px']
+	];
+
+	keys_numpad.forEach(i => $scope.numpad.push(Object.assign({}, i)));
+	keys.forEach(i => $scope.keyboard.push(Object.assign({}, i)));
 
 });

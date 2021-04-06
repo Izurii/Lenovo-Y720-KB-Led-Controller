af434b706c38f376ccec0f75e58b6506824b3c92
Izurii <heitor_massarente@msn.com>
2021-04-06 01:25:48 -0300

    Added electron-store and function to get all user profiles

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
-const { app, ipcMain, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, ipcMain, BrowserWindow, Tray, Menu } = require('electron');
 const { setKeyboardOptions } = require('./driver/index');
+
+const Store = require('electron-store');
+const path = require('path');
 
 try {
 	require('electron-reloader')(module)
 } catch (_) {}
 
 let mainWindow;
+let tray = null;
+
+const store = new Store();
 
 app.on('ready', () => {
+	
+	// tray = new Tray('./assets/icon.png');
+	// const contextMenu = Menu.buildFromTemplate([
+	// 	{ label: 'Item1', type: 'radio' },
+	// 	{ label: 'Item2', type: 'radio' },
+	// 	{ label: 'Item3', type: 'radio', checked: true },
+	// 	{ label: 'Item4', type: 'radio' }
+	// ]);
+	// tray.setToolTip('Lenovo Y720 Keyboard Backlight Controller');
+	// tray.setContextMenu(contextMenu);
+
 	mainWindow = new BrowserWindow({
+		configName: 'user-settings',
 		webPreferences: {
 			nodeIntegration: true,
 			contextIsolation: false			
@@ -19,9 +36,10 @@ app.on('ready', () => {
 		maxHeight:548,
 		maxWidth:1400,
 		autoHideMenuBar:true,
+		icon:path.join(__dirname, '/src/assets/icon.png')
 	});
 	app.allowRendererProcessReuse = false;
-	mainWindow.loadFile(path.join(__dirname, 'index.html'));
+	mainWindow.loadFile(path.join(__dirname, '/src/index.html'));
 }); 
 
 app.on('window-all-closed', () => {
@@ -38,4 +56,12 @@ app.on('activate', () => {
 
 ipcMain.on('setKB', (event, backlightMode, segmentOptions) => {
 	setKeyboardOptions(backlightMode, segmentOptions);
+});
+
+ipcMain.on('getUserProfiles', (event) => {
+	let userProfiles = {
+		selectedProfile : store.get('selectedProfile'),
+		profiles : store.get('profiles')
+	};
+	event.returnValue = userProfiles;
 });

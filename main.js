4ac74d29b47b4fd2e1a4916360423b96669285ae
Izurii <heitor_massarente@msn.com>
2021-04-08 23:52:05 -0300

    Removed auto updater

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// The current version of your app.
-const APP_VERSION = require('./package.json').version
-
-// The url that the application is going to query for new release
-const AUTO_UPDATE_URL =
-  'https://api.update.rocks/update/github.com/Izurii/Lenovo-Y720-KB-Led-Controller/stable/' + process.platform + '/' + APP_VERSION
-
 const { app, ipcMain, BrowserWindow, Tray, Menu } = require('electron');
 const { setKeyboardOptions } = require('./driver/index');
 

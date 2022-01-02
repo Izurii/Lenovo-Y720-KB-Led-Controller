d16caf986b44a2a74645bc339c9f0df573269c0e
Izurii <heitor_massarente@msn.com>
2022-01-02 07:49:05 -0300

    chore: Adicionado mais scripts para automatizar e otimizar o processod e build da aplicação

---
+++ libs/build.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+HOTKEY_ADDON_FILE=./hotkey/build/Release/hotkeyAddon.node
+LED_ADDON_FILE=./led/build/Release/ledAddon.node
+
+HOTKEY_ADDON_MD5=./hotkey/hotkeyAddon.md5
+LED_ADDON_MD5=./led/ledAddon.md5
+
+buildLedAddon() {
+	echo "Building LED addon"
+	if ! yarn run build-led-addon; then
+		echo "LED addon build failed"
+		exit 1
+	else
+		createLedAddonMD5
+	fi
+}
+
+buildHotkeyAddon() {
+	echo "Building hotkey addon"
+	if ! yarn run build-hotkey-addon; then
+		echo "Hotkey addon build failed. Stopping script"
+		exit 1
+	else
+		createHotkeyAddonMD5
+	fi
+}
+
+buildAll() {
+	buildLedAddon
+	buildHotkeyAddon
+}
+
+createMD5Files() {
+	echo "Creating MD5 files..."
+	createHotkeyAddonMD5
+	createLedAddonMD5
+	echo "MD5 files created"
+}
+
+createLedAddonMD5() {
+	echo "Creating LED addon MD5 file..."
+	md5sum ./led/binding.gyp ./led/package.json ./led/src/main.cc > $LED_ADDON_MD5
+	echo "LED addon MD5 file created"
+}
+
+createHotkeyAddonMD5() {
+	echo "Creating Hotkey addon MD5 file..."
+	md5sum ./hotkey/binding.gyp ./hotkey/package.json ./hotkey/src/main.cc > $HOTKEY_ADDON_MD5
+	echo "Hotkey addon MD5 file created"
+}
+
+if test -f "$HOTKEY_ADDON_FILE" && ! test -f "$LED_ADDON_FILE"; then
+	echo "Hotkey addon file exists, but LED addon file does not. Building LED addon"
+	buildLedAddon
+elif test -f "$LED_ADDON_FILE" && ! test -f "$HOTKEY_ADDON_FILE"; then
+	echo "LED addon file exists, but Hotkey addon file does not. Building Hotkey addon"
+	buildHotkeyAddon
+elif ! test -f "$HOTKEY_ADDON_FILE" && ! test -f "$LED_ADDON_FILE"; then
+	echo "Both addon files do not exist. Building both"
+	buildAll
+	exit 1
+else
+	echo "Both addon files exist. Checking MD5 sums"
+fi
+
+if test -f "$HOTKEY_ADDON_MD5" && test -f "$LED_ADDON_MD5"
+then
+	echo "MD5 files exist. Checking MD5sums..."
+	CHECK_MD5_HOTKEY=$(md5sum -c $HOTKEY_ADDON_MD5) > /dev/null 2>&1
+	CHECK_MD5_LED=$(md5sum -c $LED_ADDON_MD5) > /dev/null 2>&1
+
+	case "$CHECK_MD5_HOTKEY" in
+		*"WARNING"*)
+			echo "Hotkey addon is not up to date"
+			buildHotkeyAddon
+			;;
+		*)
+			echo "Hotkey addon not changed. Skipping build."
+			;;
+	esac
+
+	case "$CHECK_MD5_LED" in
+		*"WARNING"*)
+			echo "LED addon is not up to date"
+			buildLedAddon
+			;;
+		*)
+			echo "LED addon not changed. Skipping build."
+			;;
+	esac
+
+	echo "MD5sums checked. OK!"
+
+else
+	echo "MD5 files do not exist."
+	createMD5Files
+	exit 0
+fi

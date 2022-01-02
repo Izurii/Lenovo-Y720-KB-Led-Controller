0fe5eee6f0244b6aaada5fffea637fdd79f0cb3b
Izurii <heitor_massarente@msn.com>
2022-01-02 08:17:23 -0300

    chore: Centralizando scripts bash em uma única pasta e script de prestart para o app

--- scripts/libs/build.sh
+++ scripts/libs/build.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-HOTKEY_ADDON_FILE=./hotkey/build/Release/hotkeyAddon.node
-LED_ADDON_FILE=./led/build/Release/ledAddon.node
-
-HOTKEY_ADDON_MD5=./hotkey/hotkeyAddon.md5
-LED_ADDON_MD5=./led/ledAddon.md5
+# shellcheck disable=SC1091
+. ./scripts/.env
 
 buildLedAddon() {
 	echo "Building LED addon"
-	if ! yarn run build-led-addon; then
+	if ! yarn --cwd ./libs/ run build-led-addon; then
 		echo "LED addon build failed"
 		exit 1
 	else
@@ -18,7 +15,7 @@ buildLedAddon() {
 
 buildHotkeyAddon() {
 	echo "Building hotkey addon"
-	if ! yarn run build-hotkey-addon; then
+	if ! yarn --cwd ./libs/ run build-hotkey-addon; then
 		echo "Hotkey addon build failed. Stopping script"
 		exit 1
 	else
@@ -40,13 +37,13 @@ createMD5Files() {
 
 createLedAddonMD5() {
 	echo "Creating LED addon MD5 file..."
-	md5sum ./led/binding.gyp ./led/package.json ./led/src/main.cc > $LED_ADDON_MD5
+	md5sum ./led/binding.gyp ./led/package.json ./led/src/main.cc > "$LED_ADDON_MD5"
 	echo "LED addon MD5 file created"
 }
 
 createHotkeyAddonMD5() {
 	echo "Creating Hotkey addon MD5 file..."
-	md5sum ./hotkey/binding.gyp ./hotkey/package.json ./hotkey/src/main.cc > $HOTKEY_ADDON_MD5
+	md5sum ./hotkey/binding.gyp ./hotkey/package.json ./hotkey/src/main.cc > "$HOTKEY_ADDON_MD5"
 	echo "Hotkey addon MD5 file created"
 }
 
@@ -67,8 +64,8 @@ fi
 if test -f "$HOTKEY_ADDON_MD5" && test -f "$LED_ADDON_MD5"
 then
 	echo "MD5 files exist. Checking MD5sums..."
-	CHECK_MD5_HOTKEY=$(md5sum -c $HOTKEY_ADDON_MD5) > /dev/null 2>&1
-	CHECK_MD5_LED=$(md5sum -c $LED_ADDON_MD5) > /dev/null 2>&1
+	CHECK_MD5_HOTKEY=$(md5sum -c "$HOTKEY_ADDON_MD5") > /dev/null 2>&1
+	CHECK_MD5_LED=$(md5sum -c "$LED_ADDON_MD5") > /dev/null 2>&1
 
 	case "$CHECK_MD5_HOTKEY" in
 		*"WARNING"*)
---
+++ scripts/prebuild.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# shellcheck disable=SC1091
+. ./scripts/env
+
+rm -rf "$DIST_JS_DIR"
+
+yarn run build-libs
+yarn run tsc
+
+# Copying static files that don't need to be compiled
+cp -r "$SRC_DIR"/assets "$DIST_JS_DIR"/
+cp -r "$SRC_DIR"/resources "$DIST_JS_DIR"/
+cp "$SRC_DIR"/index.html "$DIST_JS_DIR"/
+
+# Copying compiled libs
+cp "$HOTKEY_LIB_RELEASE_DIR"/hotkeyAddon.node "$DIST_JS_DIR"/addons/hotkey/hotkeyAddon.node
+cp "$LED_LIB_RELEASE_DIR"/ledAddon.node "$DIST_JS_DIR"/addons/led/ledAddon.node
